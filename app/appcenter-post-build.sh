8d14648aa4864d1aa23c93059ef8b158aeee2a68
RafiMichaeli <35917992+RafiMichaeli@users.noreply.github.com>
2020-05-06 16:39:09 +0300

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -4,7 +4,6 @@ if [[ "$APPCENTER_XCODE_PROJECT" ]]; then
     -F "api_key=$TESTFAIRY_UPLOAD_API_KEY" \
     -F "file=@$APPCENTER_OUTPUT_DIRECTORY/draw-me-a-fairy.ipa" 
 fi
-
 if [[ -z "$APPCENTER_XCODE_PROJECT" ]]; then
   curl https://upload.testfairy.com/api/upload \
     -F "api_key=$TESTFAIRY_UPLOAD_API_KEY" \
