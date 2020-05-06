f4b2fe99f580b04ff68bc3074b475796c5465588
RafiMichaeli <35917992+RafiMichaeli@users.noreply.github.com>
2020-05-06 18:26:05 +0300

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-if [[ -z "$APPCENTER_XCODE_PROJECT" ]]; then
-  curl https://upload.testfairy.com/api/upload \
-    -F "api_key=$TESTFAIRY_UPLOAD_API_KEY" \
-    -F "file=@$APPCENTER_OUTPUT_DIRECTORY/app-regular-release.apk"
-fi
+
+TESTFAIRY_UPLOAD_API_KEY='2b7c2001126cbbfb221535b6e0d3728f2bf5d9f3'
+
 if [[ "$APPCENTER_XCODE_PROJECT" ]]; then
   curl https://upload.testfairy.com/api/upload \
-    -F "api_key=$TESTFAIRY_UPLOAD_API_KEY" \
-    -F "file=@$APPCENTER_OUTPUT_DIRECTORY/draw-me-a-fairy.ipa" 
+  -F "api_key=$TESTFAIRY_UPLOAD_API_KEY" \
+  -F "file=@$APPCENTER_OUTPUT_DIRECTORY/example.ipa" 
+fi
+
+if [[ -z "$APPCENTER_XCODE_PROJECT" ]]; then
+  curl https://upload.testfairy.com/api/upload \
+  -F "api_key=$TESTFAIRY_UPLOAD_API_KEY" \
+  -F "file=@$APPCENTER_OUTPUT_DIRECTORY/app-regular-release.apk"
 fi
