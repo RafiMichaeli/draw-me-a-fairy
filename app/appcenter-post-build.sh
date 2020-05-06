6920450ddbd5d6e721c4d18f176f28103a6a76ae
RafiMichaeli <35917992+RafiMichaeli@users.noreply.github.com>
2020-05-06 16:24:57 +0300

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,37 +1,12 @@
 #!/usr/bin/env bash
-
-# Release version per env
-
-ENVIRONMENT=Development
-APP_NAME=DMAF/draw-me-a-fairy
-
-# Get the message of the last commit using Git
-COMMIT_MESSAGE=$(git log -1 HEAD --pretty=format:%s)
-
 if [[ "$APPCENTER_XCODE_PROJECT" ]]; then
-
-$APPCENTER_OUTPUT_DIRECTORY/symbols/
-
-if [ "$APPCENTER_BRANCH" == "ios/staging" ]; then
--F "api_key=$TESTFAIRY_UPLOAD_API_KEY" \
--F "file=@$APPCENTER_OUTPUT_DIRECTORY/draw-me-a-fairy.ipa" \
--F "symbols_file=@$APPCENTER_OUTPUT_DIRECTORY/symbols/draw-me-a-fairy.dsym" \
-#-F testers_groups='internal_ios' \
-#-F notify='on'
--F 'release_notes=$COMMIT_MESSAGE'
--F tags='appcntr, ios'
-# else
+  curl https://upload.testfairy.com/api/upload \
+    -F "api_key=$TESTFAIRY_UPLOAD_API_KEY" \
+    -F "file=@$APPCENTER_OUTPUT_DIRECTORY/draw-me-a-fairy.ipa" 
 fi
-fi
+
 if [[ -z "$APPCENTER_XCODE_PROJECT" ]]; then
-if [ "$APPCENTER_BRANCH" == "android/staging" ]; then
-curl https://upload.testfairy.com/api/upload \
--F "api_key=$TESTFAIRY_UPLOAD_API_KEY" \
--F "file=@$APPCENTER_OUTPUT_DIRECTORY/app-staging.apk" \
-# -F symbols_file=@sample_mapping.txt \
-#-F testers_groups='internal_android' \
-#-F notify='on'
--F 'release_notes=$COMMIT_MESSAGE'
--F tags='appcntr, android'
-fi
+  curl https://upload.testfairy.com/api/upload \
+    -F "api_key=$TESTFAIRY_UPLOAD_API_KEY" \
+    -F "file=@$APPCENTER_OUTPUT_DIRECTORY/app-staging.apk"
 fi
